9b28ecfd9572746ec309945c43415fd030a0d281
thakursaurabh1998 <thakursaurabh1998@gmail.com>
2018-03-03 16:13:20 +0530

    final done

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
-
 let notes = [];
-let count = 0;
 
-
-// completing note
-const completeNote = (object,reverseID) => {
-	object.todo()[reverseID].completed(!object.todo()[reverseID].completed());
-};
 
 // viewModel
 const viewModel = function() {
 	// Observables
 	this.entry = ko.observable();
 	this.todo = ko.observableArray([]);
+	this.left = ko.observable(0);
+	this.tickAll = ko.observable(false);
+	this.selectedA = ko.observable(true);
+	this.selectedActive = ko.observable(false);
+	this.selectedC = ko.observable(false);
+	this.showLeft = ko.observable(0);
 
 	// Class to create new note
 	class note {
@@ -21,7 +20,6 @@ const viewModel = function() {
 			this.completed = ko.observable(false);
 			this.editing = ko.observable(false);
 			this.display = ko.observable(true);
-			this.id = ko.observable(count++);
 		}
 	}
 
@@ -39,16 +37,13 @@ const viewModel = function() {
 			this.todo.unshift(newNote);
 			this.entry('');
 		}
-		else{
+		else
 			return true;
-		}
 	};
 
 	// completing a todo
 	this.callCompleteNote = (clickedNote) => {
-		const n = this.todo().length - clickedNote.id() - 1;
-		completeNote(this,n);
-		console.log(clickedNote.info());
+		clickedNote.completed(!clickedNote.completed());
 		return true;
 	};
 
@@ -60,6 +55,10 @@ const viewModel = function() {
 	// delete (remove display)
 	this.callDeleteNote = (clickedNote) => {
 		clickedNote.display(!clickedNote.display());
+		clickedNote.completed(true);
+		const n=this.todo().indexOf(clickedNote)
+		this.todo().splice(n,1);
+		this.showLeft(this.todo().length);
 		return true;
 	};
 
@@ -73,20 +72,84 @@ const viewModel = function() {
 
 	// show footer and toggle-all
 	this.showAll = ko.computed(() => {
-		let flag = 1;
-		if(this.todo().length===0)
+		if(this.showLeft()===0)
 			return false;
-		for(const a of this.todo()){
-			if(a.display()){
-				flag=0;
-				break;
-			}
-		}
-		if(flag)
-			return true;
 		else
-			return false;
+			return true;
 	});
+
+	// Item left computed
+	this.itemLeft = ko.computed(() => {
+		let left=0;
+		let total=0;
+		for(let a=0;a<this.todo().length;a++){
+			total++;
+		}
+		for(const l of this.todo()){
+			if(l.completed()===false)
+				left++;
+		}
+		this.showLeft(total);
+		this.left(left);
+	});
+
+	// Mark all completed
+	this.markAllComplete = () => {
+		this.tickAll(!this.tickAll());
+		for(const a of this.todo())
+			if(this.tickAll())
+				a.completed(true);
+			else
+				a.completed(false);
+	};
+
+	// Clear all completed todos
+	this.clearCompleted = () => {
+		for(let a=0;a<this.todo().length;a++)
+			if(this.todo()[a].completed()===true){
+				this.todo()[a].display(false);
+				this.todo().splice(a,1);
+				a--;
+			}
+		this.showLeft(this.todo().length);
+		this.tickAll(false);
+	};
+
+	// Show all todos
+	this.showAllTodo = () => {
+		for(const a of this.todo())
+			a.display(true);
+		this.selectedA(true);
+		this.selectedC(false);
+		this.selectedActive(false);
+		return true;
+	};
+
+	// Show active todos
+	this.showActive = () => {
+		for(const a of this.todo())
+			if(a.completed()===false)
+				a.display(true);
+			else
+				a.display(false);
+		this.selectedA(false);
+		this.selectedC(false);
+		this.selectedActive(true);
+		return true;
+	};
+
+	// Show completed todos
+	this.showCompleted = () => {
+		for(const a of this.todo())
+			if(a.completed()===true)
+				a.display(true);
+			else
+				a.display(false);
+		this.selectedA(false);
+		this.selectedC(true);
+		this.selectedActive(false);
+		return true;
+	};
 };
 
 
